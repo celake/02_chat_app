e042a565d8a9ecc7fdc1f1261f699dd54ad0b906
celake <me@charlottelake.eu>
2025-10-09 15:55:53 +0100

    feat: implement auth controllers for user registration, login, logout, profile update, and token check

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.routes.js';
@@ -9,7 +10,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.json());
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+
 
 
 connectDB().then( () => {
