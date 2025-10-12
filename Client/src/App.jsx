4cd8ec46a207e3c982d17925cb0d5f6604091302
celake <me@charlottelake.eu>
2025-10-12 16:10:06 +0100

    add react router with basic page components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router";
 
+import Navbar from "./components/Navbar";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
+import SettingsPage from "./Pages/SettingsPage";
 
 export default function App() {
   return (
-    <div className="text-red-500">App</div>
+    <div>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/signup" element={<SignUpPage />}/>
+            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/profile" element={<ProfilePage />}/>
+            <Route path="/settings" element={<SettingsPage />}/>
+        </Routes>
+    </div>
   )
 }
 
