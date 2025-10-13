5a542aff67890b3db8e46433d530c970644ed532
celake <me@charlottelake.eu>
2025-10-13 11:01:25 +0100

    feat: add CORS and route protection for authenticated users

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,8 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
+import { Loader } from 'lucide-react';
+import { useEffect } from 'react';
 
+import { useAuthStore } from "./store/useAuthStore";
 import Navbar from "./components/Navbar";
 import HomePage from "./Pages/HomePage";
 import SignUpPage from "./Pages/SignUpPage";
@@ -8,14 +11,28 @@ import ProfilePage from "./Pages/ProfilePage";
 import SettingsPage from "./Pages/SettingsPage";
 
 export default function App() {
+    const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+    useEffect(() => {
+      checkAuth();
+    }, [checkAuth]);
+
+    console.log( { authUser });
+
+    if (isCheckingAuth && !authUser) return (
+        <div className="flex items-center ustify-center h-screen">
+            <Loader className="size-10 animate-spin" />
+        </div>
+    )
+    
   return (
     <div>
         <Navbar />
         <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/signup" element={<SignUpPage />}/>
-            <Route path="/login" element={<LoginPage />}/>
-            <Route path="/profile" element={<ProfilePage />}/>
+            <Route path="/" element={authUser ? <HomePage /> : <Navigate to='/login'/> } />
+            <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={'/'} /> }/>
+            <Route path="/login" element={ !authUser ?<LoginPage /> : <Navigate to={'/'} /> }/>
+            <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to='/login'/>} />
             <Route path="/settings" element={<SettingsPage />}/>
         </Routes>
     </div>
--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.routes.js';
@@ -14,6 +15,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhose:5273",
+    credentials: true,
+}))
 
 
 app.use("/api/auth", authRoutes);
