ec47486af7e84d94478ab04a2c3284b57a922a99
celake <me@charlottelake.eu>
2025-10-15 15:56:45 +0100

    feat: add Navbar component shared across all pages

--- Client/src/store/useAuthStore.js
+++ Client/src/store/useAuthStore.js
@@ -33,5 +33,16 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({isSigningUp: false})
         }
+    },
+
+    logout: async () => {
+        try {
+          await axiosInstance.post("auth/logout");
+          set({ autUser: null});  
+          toast.success("Logged out successfully")
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     }
-}));
+})
+);
